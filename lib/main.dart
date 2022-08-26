321d6c57525c8d0f7469ab8508f3961342ad7853
keizo <13keizo@gmail.com>
2022-08-26 09:05:07 -0400

    corrigido

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:incontre/pages/login.dart';
+import 'package:incontre/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Login(),
+      home: Splash(),
     );
   }
 }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:incontre/pages/homepage.dart';
+import 'package:incontre/pages/login.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Login()));
+  }
 
   @override
   Widget build(BuildContext context) {
